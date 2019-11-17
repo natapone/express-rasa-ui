5b45ccb049fed164df6960d05e61f944d62acac6
Natapone Charsombut <natapone@gmail.com>
2019-11-17 21:13:18 +0630

    add ui file

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 
 app.use('/static', express.static('public'))
+app.use('/ui', express.static('ui'))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
