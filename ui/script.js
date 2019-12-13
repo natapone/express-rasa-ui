a3789b46ff8e4def42e02d662778412c9e9e6ed7
Natapone Charsombut <natapone@gmail.com>
2019-12-13 16:21:00 +0630

    replace tab with space

--- ui/script.js
+++ ui/script.js
@@ -1,213 +1,213 @@
 $(document).ready(function () {
 
-	//Widget Code
-	var bot = '<div class="chatCont" id="chatCont">' +
-		'<div class="bot_profile">' +
-		'<img src="logo.png" class="bot_p_img">' +
-		'<div class="close">' +
-		'<i class="fa fa-times" aria-hidden="true"></i>' +
-		'</div>' +
-		'</div><!--bot_profile end-->' +
-		'<div id="result_div" class="resultDiv"></div>' +
-		'<div class="chatForm" id="chat-div">' +
-		'<div class="spinner">' +
-		'<div class="bounce1"></div>' +
-		'<div class="bounce2"></div>' +
-		'<div class="bounce3"></div>' +
-		'</div>' +
-		'<input type="text" id="chat-input" autocomplete="off" placeholder="Start Typing here..."' + 'class="form-control bot-txt"/>' +
-		'</div>' +
-		'</div><!--chatCont end-->' +
+  //Widget Code
+  var bot = '<div class="chatCont" id="chatCont">' +
+    '<div class="bot_profile">' +
+    '<img src="logo.png" class="bot_p_img">' +
+    '<div class="close">' +
+    '<i class="fa fa-times" aria-hidden="true"></i>' +
+    '</div>' +
+    '</div><!--bot_profile end-->' +
+    '<div id="result_div" class="resultDiv"></div>' +
+    '<div class="chatForm" id="chat-div">' +
+    '<div class="spinner">' +
+    '<div class="bounce1"></div>' +
+    '<div class="bounce2"></div>' +
+    '<div class="bounce3"></div>' +
+    '</div>' +
+    '<input type="text" id="chat-input" autocomplete="off" placeholder="Start Typing here..."' + 'class="form-control bot-txt"/>' +
+    '</div>' +
+    '</div><!--chatCont end-->' +
 
-		'<div class="profile_div">' +
-		'<div class="row">' +
-		'<div class="col-hgt col-sm-offset-2">' +
-		'<img src="logo.png" class="img-circle img-profile">' +
-		'</div><!--col-hgt end-->' +
-		'<div class="col-hgt">' +
-		'<div class="chat-txt">' +
-		'' +
-		'</div>' +
-		'</div><!--col-hgt end-->' +
-		'</div><!--row end-->' +
-		'</div><!--profile_div end-->';
+    '<div class="profile_div">' +
+    '<div class="row">' +
+    '<div class="col-hgt col-sm-offset-2">' +
+    '<img src="logo.png" class="img-circle img-profile">' +
+    '</div><!--col-hgt end-->' +
+    '<div class="col-hgt">' +
+    '<div class="chat-txt">' +
+    '' +
+    '</div>' +
+    '</div><!--col-hgt end-->' +
+    '</div><!--row end-->' +
+    '</div><!--profile_div end-->';
 
-	$("mybot").html(bot);
+  $("mybot").html(bot);
 
-	// ------------------------------------------ Toggle chatbot -----------------------------------------------
-	//function to click and open chatbot from icon
-	$('.profile_div').click(function () {
-		$('.profile_div').toggle();
-		$('.chatCont').toggle();
-		$('.bot_profile').toggle();
-		$('.chatForm').toggle();
-		document.getElementById('chat-input').focus();
-	});
+  // ------------------------------------------ Toggle chatbot -----------------------------------------------
+  //function to click and open chatbot from icon
+  $('.profile_div').click(function () {
+    $('.profile_div').toggle();
+    $('.chatCont').toggle();
+    $('.bot_profile').toggle();
+    $('.chatForm').toggle();
+    document.getElementById('chat-input').focus();
+  });
 
-	//function to click and close chatbot to icon
-	$('.close').click(function () {
-		$('.profile_div').toggle();
-		$('.chatCont').toggle();
-		$('.bot_profile').toggle();
-		$('.chatForm').toggle();
-	});
-
-
-
-
-	// on input/text enter--------------------------------------------------------------------------------------
-
-	$('#chat-input').on('keyup keypress', function (e) {
-		var keyCode = e.keyCode || e.which;
-		var text = $("#chat-input").val();
-		if (keyCode === 13) {
-			if (text == "" || $.trim(text) == '') {
-				e.preventDefault();
-				return false;
-			} else {
-				$("#chat-input").blur();
-				setUserResponse(text);
-				send(text);
-				e.preventDefault();
-				return false;
-			}
-		}
-	});
-
-
-	//------------------------------------------- Call the RASA API--------------------------------------
-	function send(text) {
-
-
-		$.ajax({
-			url: 'http://localhost:5002/webhooks/rest/webhook', //  RASA API
-			type: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			data: JSON.stringify({
-				"sender": "user",
-				"message": text
-			}),
-			success: function (data, textStatus, xhr) {
-				// console.log(data);
-				// console.log(Object.keys(data).length);
-
-				if (Object.keys(data).length !== 0) {
-					// Loop each return
-					for (i = 0; i < Object.keys(data).length; i++) {
-						var data_detail = data[i];
-
-						// Loop each attribute
-						Object.keys(data_detail).forEach(function(key) {
-							var value = data_detail[key];
-							if (key == "buttons" && value) {
-								// console.log("---- " + key +": "+ value);
-								addSuggestion(value)
-							}
-						})
-
-					}
-				}
-
-				setBotResponse(data);
-
-			},
-			error: function (xhr, textStatus, errorThrown) {
-				console.log('Error in Operation');
-				setBotResponse('error');
-			}
-		});
+  //function to click and close chatbot to icon
+  $('.close').click(function () {
+    $('.profile_div').toggle();
+    $('.chatCont').toggle();
+    $('.bot_profile').toggle();
+    $('.chatForm').toggle();
+  });
 
 
 
 
+  // on input/text enter--------------------------------------------------------------------------------------
 
-	}
-
-
-	//------------------------------------ Set bot response in result_div -------------------------------------
-	function setBotResponse(val) {
-		setTimeout(function () {
-
-			if ($.trim(val) == '' || val == 'error') { //if there is no response from bot or there is some error
-				val = 'Sorry I wasn\'t able to understand your Query. Let\' try something else!'
-				var BotResponse = '<p class="botResult">' + val + '</p><div class="clearfix"></div>';
-				$(BotResponse).appendTo('#result_div');
-			} else {
-
-				//if we get message from the bot succesfully
-				var msg = "";
-				for (var i = 0; i < val.length; i++) {
-					if (val[i]["image"]) { //check if there are any images
-						msg += '<p class="botResult"><img  width="200" height="124" src="' + val[i].image + '/"></p><div class="clearfix"></div>';
-					} else {
-						msg += '<p class="botResult">' + val[i].text + '</p><div class="clearfix"></div>';
-					}
-
-				}
-				BotResponse = msg;
-				$(BotResponse).appendTo('#result_div');
-			}
-			scrollToBottomOfResults();
-			hideSpinner();
-		}, 500);
-	}
+  $('#chat-input').on('keyup keypress', function (e) {
+    var keyCode = e.keyCode || e.which;
+    var text = $("#chat-input").val();
+    if (keyCode === 13) {
+      if (text == "" || $.trim(text) == '') {
+        e.preventDefault();
+        return false;
+      } else {
+        $("#chat-input").blur();
+        setUserResponse(text);
+        send(text);
+        e.preventDefault();
+        return false;
+      }
+    }
+  });
 
 
-	//------------------------------------- Set user response in result_div ------------------------------------
-	function setUserResponse(val) {
-		var UserResponse = '<p class="userEnteredText">' + val + '</p><div class="clearfix"></div>';
-		$(UserResponse).appendTo('#result_div');
-		$("#chat-input").val('');
-		scrollToBottomOfResults();
-		showSpinner();
-		$('.suggestion').remove();
-	}
+  //------------------------------------------- Call the RASA API--------------------------------------
+  function send(text) {
 
 
-	//---------------------------------- Scroll to the bottom of the results div -------------------------------
-	function scrollToBottomOfResults() {
-		var terminalResultsDiv = document.getElementById('result_div');
-		terminalResultsDiv.scrollTop = terminalResultsDiv.scrollHeight;
-	}
+    $.ajax({
+      url: 'http://localhost:5002/webhooks/rest/webhook', //  RASA API
+      type: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: JSON.stringify({
+        "sender": "user",
+        "message": text
+      }),
+      success: function (data, textStatus, xhr) {
+        // console.log(data);
+        // console.log(Object.keys(data).length);
+
+        if (Object.keys(data).length !== 0) {
+          // Loop each return
+          for (i = 0; i < Object.keys(data).length; i++) {
+            var data_detail = data[i];
+
+            // Loop each attribute
+            Object.keys(data_detail).forEach(function(key) {
+              var value = data_detail[key];
+              if (key == "buttons" && value) {
+                // console.log("---- " + key +": "+ value);
+                addSuggestion(value)
+              }
+            })
+
+          }
+        }
+
+        setBotResponse(data);
+
+      },
+      error: function (xhr, textStatus, errorThrown) {
+        console.log('Error in Operation');
+        setBotResponse('error');
+      }
+    });
 
 
-	//---------------------------------------- Spinner ---------------------------------------------------
-	function showSpinner() {
-		$('.spinner').show();
-	}
-
-	function hideSpinner() {
-		$('.spinner').hide();
-	}
 
 
 
-
-	//------------------------------------------- Buttons(suggestions)--------------------------------------------------
-	function addSuggestion(textToAdd) {
-		setTimeout(function () {
-			var suggestions = textToAdd;
-			var suggLength = textToAdd.length;
-			$('<p class="suggestion"></p>').appendTo('#result_div');
-			// Loop through suggestions
-			for (i = 0; i < suggLength; i++) {
-				$('<span class="sugg-options">' + suggestions[i].title + '</span>').appendTo('.suggestion');
-			}
-			scrollToBottomOfResults();
-		}, 1000);
-	}
+  }
 
 
-	// on click of suggestions get value and send to API.AI
-	$(document).on("click", ".suggestion span", function () {
-		var text = this.innerText;
-		setUserResponse(text);
-		send(text);
-		$('.suggestion').remove();
-	});
-	// Suggestions end -----------------------------------------------------------------------------------------
+  //------------------------------------ Set bot response in result_div -------------------------------------
+  function setBotResponse(val) {
+    setTimeout(function () {
+
+      if ($.trim(val) == '' || val == 'error') { //if there is no response from bot or there is some error
+        val = 'Sorry I wasn\'t able to understand your Query. Let\' try something else!'
+        var BotResponse = '<p class="botResult">' + val + '</p><div class="clearfix"></div>';
+        $(BotResponse).appendTo('#result_div');
+      } else {
+
+        //if we get message from the bot succesfully
+        var msg = "";
+        for (var i = 0; i < val.length; i++) {
+          if (val[i]["image"]) { //check if there are any images
+            msg += '<p class="botResult"><img  width="200" height="124" src="' + val[i].image + '/"></p><div class="clearfix"></div>';
+          } else {
+            msg += '<p class="botResult">' + val[i].text + '</p><div class="clearfix"></div>';
+          }
+
+        }
+        BotResponse = msg;
+        $(BotResponse).appendTo('#result_div');
+      }
+      scrollToBottomOfResults();
+      hideSpinner();
+    }, 500);
+  }
+
+
+  //------------------------------------- Set user response in result_div ------------------------------------
+  function setUserResponse(val) {
+    var UserResponse = '<p class="userEnteredText">' + val + '</p><div class="clearfix"></div>';
+    $(UserResponse).appendTo('#result_div');
+    $("#chat-input").val('');
+    scrollToBottomOfResults();
+    showSpinner();
+    $('.suggestion').remove();
+  }
+
+
+  //---------------------------------- Scroll to the bottom of the results div -------------------------------
+  function scrollToBottomOfResults() {
+    var terminalResultsDiv = document.getElementById('result_div');
+    terminalResultsDiv.scrollTop = terminalResultsDiv.scrollHeight;
+  }
+
+
+  //---------------------------------------- Spinner ---------------------------------------------------
+  function showSpinner() {
+    $('.spinner').show();
+  }
+
+  function hideSpinner() {
+    $('.spinner').hide();
+  }
+
+
+
+
+  //------------------------------------------- Buttons(suggestions)--------------------------------------------------
+  function addSuggestion(textToAdd) {
+    setTimeout(function () {
+      var suggestions = textToAdd;
+      var suggLength = textToAdd.length;
+      $('<p class="suggestion"></p>').appendTo('#result_div');
+      // Loop through suggestions
+      for (i = 0; i < suggLength; i++) {
+        $('<span class="sugg-options">' + suggestions[i].title + '</span>').appendTo('.suggestion');
+      }
+      scrollToBottomOfResults();
+    }, 1000);
+  }
+
+
+  // on click of suggestions get value and send to API.AI
+  $(document).on("click", ".suggestion span", function () {
+    var text = this.innerText;
+    setUserResponse(text);
+    send(text);
+    $('.suggestion').remove();
+  });
+  // Suggestions end -----------------------------------------------------------------------------------------
 
 
 });
