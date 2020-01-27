74d8c96d78751d0a3c8c403e887dcb375ab41e60
Natapone Charsombut <natapone@gmail.com>
2020-01-27 11:12:03 +0630

    add chat button to new page

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const cors = require('cors')
+const favicon = require('serve-favicon')
+const path = require('path')
 const app = express()
 const port = process.env.PORT || 3000;
 
 app.use('/static', express.static('public'))
 app.use('/ui', express.static('ui'))
+app.use('/tmm', express.static('tmm'))
+// app.use('/example', express.static('example'))
+
 app.use(cors())
+app.use(favicon(path.join(__dirname, 'public/image', 'favicon.ico')))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
---
+++ tmm/chatbot.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+
+  // --- Toggle chatbot ---
+  $('.kmbot_chat_button').click(function () {
+    console.log("Khin Min bot clicked!!");
+  });
+
+});
